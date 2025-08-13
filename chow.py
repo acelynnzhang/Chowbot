4cc0c4d416aed13c0ec64d51ebe0439765875332
Acelynn Zhang <azyx0987e@gmail.com>
2025-08-13 13:41:48 -0500

    no sleep

--- chow.py
+++ chow.py
@@ -10,36 +10,34 @@ load_dotenv()
 
 chow_url = os.getenv('CHOW_URL')
 
-hours = 0
-goal = 0
 
-while True:
-    if hours == goal:
-            
-        url = "http://api.giphy.com/v1/gifs/random"
-
-        params = parse.urlencode({
-        "tag": "job application",
-        "api_key": os.getenv('KEY')
-        })
-
-        response = requests.get("".join((url, "?", params)))
-
-        gif = response.json()['data']['images']['downsized_large']['url']
-
-        chowmsg = {
-                    "content" : f"<@{os.getenv('CHOW_ID')}>",
-                    "embeds":[{"image":{"url":f"{gif}"}}]
-                }
-
-        result = requests.post(chow_url, json = chowmsg)
-
-        goal = random.randint(1, 72)
-        hours = 0
-
-    time.sleep(3600)
-    hours += 1
-
-
+num = random.random()
+print(num)
+if  num > 0.95:
         
+    url = "http://api.giphy.com/v1/gifs/random"
+
+    params = parse.urlencode({
+    "tag": "job application",
+    "api_key": os.getenv('KEY')
+    })
+
+    response = requests.get("".join((url, "?", params)))
+
+    gif = response.json()['data']['images']['downsized_large']['url']
+
+    chowmsg = {
+                "content" : f"<@{os.getenv('CHOW_ID')}>",
+                "embeds":[{"image":{"url":f"{gif}"}}]
+            }
+
+    result = requests.post(chow_url, json = chowmsg)
+
+    goal = random.randint(1, 72)
+    hours = 0
+
+
+
+
+    
 
